3688207d71f1d0b5b395bca79a370e5f539f29fc
Sam Verschueren <sam.verschueren@gmail.com>
2015-04-04 09:41:10 +0200

    Credentials can now be set via setters or provided via the options object.

--- index.js
+++ index.js
@@ -14,17 +14,36 @@ var path = require('path'),
     nodemailer = require('nodemailer'),
     ses = require('nodemailer-ses-transport');
 
+var credentials = {
+    accessKeyId: undefined,
+    secretAccessKey: undefined
+};
+
 // Export the plugin
 module.exports = function(options) {
 
-    // TODO try to pass AWS credentials via setters instead of via the options
+    // First take the credentials that are set by the setters, if those are not defined, take
+    // the credentials in the options.
+    credentials.accessKeyId = credentials.accessKeyId || options.accessKeyId;
+    credentials.secretAccessKey = credentials.secretAccessKey || options.secretAccessKey;
+
     var attachments = [],
         transporter = nodemailer.createTransport(ses({
-            accessKeyId: options.accessKeyId,
-            secretAccessKey: options.secretAccessKey
+            accessKeyId: credentials.accessKeyId,
+            secretAccessKey: credentials.secretAccessKey
         }));
 
     return through.obj(function(file, enc, cb) {
+        if(credentials.accessKeyId === undefined) {
+            // Return an error if no accessKeyId is specified
+            cb(new gutil.PluginError('gulp-ses', 'Please provide an access key ID.'));
+        }
+
+        if(credentials.secretAccessKey === undefined) {
+            // Return an error if no secretAccessKey is specified
+            cb(new gutil.PluginError('gulp-ses', 'Please provide a secret access key'));
+        }
+
         if(file.isDirectory()) {
             // Directories are not supported. They should be zipped first.
             cb(new gutil.PluginError('gulp-ses', 'Directories not supported. Please zip the directory first before mailing it.'));
@@ -53,4 +72,22 @@ module.exports = function(options) {
             cb(err);
         });
     });
+};
+
+/**
+ * Sets the Amazon access key ID that is used to authorize with AWS.
+ *
+ * @param {String} accessKeyId The AWS access key ID.
+ */
+module.exports.setAccessKeyId = function(accessKeyId) {
+    credentials.accessKeyId = accessKeyId;
+};
+
+/**
+ * Sets the Amazon secret access key that is used to authorize with AWS.
+ *
+ * @param {String} secretAccessKey The AWS secret access key
+ */
+module.exports.setSecretAccessKey = function(secretAccessKey) {
+    credentials.secretAccessKey = secretAccessKey;
 };
